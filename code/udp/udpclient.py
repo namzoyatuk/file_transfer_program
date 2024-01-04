b95cbac8261aede8b46ec3b74ac46da98f8d049b
namzoyatuk <kutayozman@gmail.com>
2024-01-05 01:13:19 +0300

    receive_file method have been implemented

--- code/udp/udpclient.py
+++ code/udp/udpclient.py
@@ -21,68 +21,68 @@ def reset_for_next_file():
 
 
 
+
+
+def receive_file():
+    try:
+        while True:
+            # Receive packet from the server
+            message, address = UDPClientSocket.recvfrom(bufferSize)
+            packet = message.decode()
+
+            # Check for the end of the file transfer
+            if packet == 'END':
+                print("File transfer completed for one file.")
+                # with open('received_file.obj', 'wb') as f:  # Change the file name or add a counter to distinguish files
+                #     f.writelines(file_data)
+                # print("Received file saved.")
+                reset_for_next_file()  # Prepare for the next file
+
+            # Extract sequence number and data from the packet
+            seq, data = packet.split(':', 1)
+            seq = int(seq)
+
+            # Send acknowledgment back to the server
+            UDPClientSocket.sendto(str(seq).encode(), address)
+
+            # If this is the next expected packet, save the data
+            if seq == expected_seq:
+                file_data.append(data)
+                expected_seq += 1
+
+                # Check for any buffered packets
+                while expected_seq in received_packets:
+                    file_data.append(received_packets.pop(expected_seq))
+                    expected_seq += 1
+            else:
+                # Buffer out-of-order packets
+                received_packets[seq] = data
+
+
+
+    except KeyboardInterrupt:
+        print("File transfer interrupted.")
+
+    finally:
+        # Save the received data to a file
+        with open('receivedudp.obj', 'w') as f:
+            for data in file_data:
+                f.write(data)
+        print("Received file saved as 'receivedudp2.obj'")
+
+
 first_file_name = input("Please enter the file number: ")
 UDPClientSocket.sendto(first_file_name.encode(), serverAddressPort)
 
-# Prepare to receive file
-received_packets = {}
-expected_seq = 0
-file_data = []
+while True:
+    # Prepare to receive file
+    received_packets = {}
+    expected_seq = 0
+    file_data = []
 
-small_and_large = 0
+    receive_file()
+    receive_file()
 
-try:
-    while True:
-        # Receive packet from the server
-        if small_and_large == 2:
-            # a flag for both small and large object has been sent and
-            # asks for new file input
-            first_file_name = input("Please enter the file number: ")
-            UDPClientSocket.sendto(first_file_name.encode(), serverAddressPort) # a random message to kill daemon thread
-            UDPClientSocket.sendto(first_file_name.encode(), serverAddressPort)
-            small_and_large = 0
-
-        message, address = UDPClientSocket.recvfrom(bufferSize)
-        packet = message.decode()
-
-        # Check for the end of the file transfer
-        if packet == 'END':
-            print("File transfer completed for one file.")
-            # with open('received_file.obj', 'wb') as f:  # Change the file name or add a counter to distinguish files
-            #     f.writelines(file_data)
-            # print("Received file saved.")
-            reset_for_next_file()  # Prepare for the next file
-            small_and_large += 1
-            continue  # Continue to receive the next file
-
-        # Extract sequence number and data from the packet
-        seq, data = packet.split(':', 1)
-        seq = int(seq)
-
-        # Send acknowledgment back to the server
-        UDPClientSocket.sendto(str(seq).encode(), address)
-
-        # If this is the next expected packet, save the data
-        if seq == expected_seq:
-            file_data.append(data)
-            expected_seq += 1
-
-            # Check for any buffered packets
-            while expected_seq in received_packets:
-                file_data.append(received_packets.pop(expected_seq))
-                expected_seq += 1
-        else:
-            # Buffer out-of-order packets
-            received_packets[seq] = data
-
-
-
-except KeyboardInterrupt:
-    print("File transfer interrupted.")
-
-finally:
-    # Save the received data to a file
-    with open('receivedudp.obj', 'w') as f:
-        for data in file_data:
-            f.write(data)
-    print("Received file saved as 'receivedudp2.obj'")
+    first_file_name = input("Please enter the file number: ")
+    UDPClientSocket.sendto(first_file_name.encode(), serverAddressPort) # For killing the daemon thread
+    UDPClientSocket.sendto(first_file_name.encode(), serverAddressPort)
