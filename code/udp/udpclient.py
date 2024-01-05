550deb88981e6047ead60aa6b31abaab36225b33
namzoyatuk <kutayozman@gmail.com>
2024-01-05 16:24:10 +0300

    bug fix

--- code/udp/udpclient.py
+++ code/udp/udpclient.py
@@ -40,11 +40,11 @@ def integrity_check(file_path, hash_path):
 def end_of_file_handling(small_and_large, file_name, file_data):
 
     print("END OF FILE HANDLING WORKING")
-    print(file_data)
     if small_and_large == 0:
         print("File transfer completed for small-" + file_name + ".obj")
         with open("small-" + file_name + ".obj", 'w') as f:
             for data in file_data:
+                print(data)
                 f.write(data)
         print("Received file saved.")
         # Prepare for the next file
